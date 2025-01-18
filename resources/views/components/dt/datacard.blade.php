97b4a6c455ca14eeabf3162f8073767099ee8b15
Shavana Alif <85614368+krealif@users.noreply.github.com>
2025-01-18 03:53:38 +0000

    fix: datacard layout when collection empty

--- resources/views/components/dt/datacard.blade.php
+++ resources/views/components/dt/datacard.blade.php
@@ -10,58 +10,56 @@
       ->count();
 @endphp
 
-@if ($collection->count() > 0)
-  <div id="dt-datatable">
-    <div class="d-flex flex-column flex-md-row justify-content-between">
-      @if ($search)
-        <form id="dt-search" class="col-12 col-md-6 col-lg-4">
-          <label for="search" class="visually-hidden">Pencarian</label>
-          <div class="row g-2">
-            <div class="col">
-              <div class="input-group">
-                <x-form.input id="search" name="{{ $search }}" type="text" :showError=false value="{{ request()->filter[$search] ?? '' }}"
-                  placeholder="{{ $searchPlaceholder ?? 'Pencarian' }}" />
-                @if (isset(request()->filter[$search]))
-                  <button type="button" id="dt-btn-clear" class="btn btn-icon">
-                    <x-lucide-x class="icon text-red" defer />
-                  </button>
-                @endif
-              </div>
-            </div>
-            <div class="col-auto">
-              <button type="submit" class="btn">
-                <x-lucide-search class="icon" />
-                Cari
-              </button>
+<div id="dt-datatable">
+  <div class="d-flex flex-column flex-md-row justify-content-between">
+    @if ($search)
+      <form id="dt-search" class="col-12 col-md-6 col-lg-4">
+        <label for="search" class="visually-hidden">Pencarian</label>
+        <div class="row g-2">
+          <div class="col">
+            <div class="input-group">
+              <x-form.input id="search" name="{{ $search }}" type="text" :showError=false value="{{ request()->filter[$search] ?? '' }}"
+                placeholder="{{ $searchPlaceholder ?? 'Pencarian' }}" />
+              @if (isset(request()->filter[$search]))
+                <button type="button" id="dt-btn-clear" class="btn btn-icon">
+                  <x-lucide-x class="icon text-red" defer />
+                </button>
+              @endif
             </div>
           </div>
-        </form>
-      @endif
-      @if (isset($filterForm) || isset($actions))
-        <div @class(['col-12 col-md-auto', 'mt-md-0 mt-3' => $search])>
-          <div class="btn-list">
-            @isset($filterForm)
-              <button type="button" class="btn btn-filter collapsed" data-bs-toggle="collapse" data-bs-target="#{{ $collapseFilterId = uniqid() }}" aria-expanded="false"
-                aria-controls="false">
-                <x-lucide-filter class="icon" />
-                Filter
-                @if ($totalFilter)
-                  <span class="badge bg-blue-lt ms-2">{{ $totalFilter }}</span>
-                @endif
-              </button>
-            @endisset
-            {{ $actions ?? '' }}
+          <div class="col-auto">
+            <button type="submit" class="btn">
+              <x-lucide-search class="icon" />
+              Cari
+            </button>
           </div>
         </div>
-      @endif
-    </div>
-    @isset($filterForm)
-      <div class="collapse mt-2 mt-md-3" id="{{ $collapseFilterId }}">
-        <form id="dt-filter" class="card card-mafindo">
-          <div class="card-body">
-            {{ $filterForm }}
-          </div>
-          <div class="card-footer bg-white">
+      </form>
+    @endif
+    @if (isset($filterForm) || isset($actions))
+      <div @class(['col-12 col-md-auto', 'mt-md-0 mt-3' => $search])>
+        <div class="btn-list">
+          @isset($filterForm)
+            <button type="button" class="btn btn-filter collapsed" data-bs-toggle="collapse" data-bs-target="#{{ $collapseFilterId = uniqid() }}" aria-expanded="false"
+              aria-controls="false">
+              <x-lucide-filter class="icon" />
+              Filter
+              @if ($totalFilter)
+                <span class="badge bg-blue-lt ms-2">{{ $totalFilter }}</span>
+              @endif
+            </button>
+          @endisset
+          {{ $actions ?? '' }}
+        </div>
+      </div>
+    @endif
+  </div>
+  @isset($filterForm)
+    <div class="collapse mt-2 mt-md-3" id="{{ $collapseFilterId }}">
+      <form id="dt-filter" class="card card-mafindo">
+        <div class="card-body">
+          {{ $filterForm }}
+          <div class="btn-list mt-4">
             <button type="submit" class="btn btn-primary me-2">
               Terapkan
             </button>
@@ -71,16 +69,25 @@
               </button>
             @endif
           </div>
-        </form>
-      </div>
-    @endisset
-    <div class="row row-deck row-cards mt-2">
-      {{ $slot }}
+        </div>
+      </form>
     </div>
+  @endisset
+  @if ($collection->isNotEmpty())
+    <div class="mt-3">
+      <div class="row row-deck row-cards">
+        {{ $slot }}
+      </div>
+    </div>
+  @endif
+</div>
+@if (method_exists($collection, 'hasPages') && $collection->hasPages())
+  <div class="mt-3 pt-2 border-top">
+    {{ $collection->links() }}
   </div>
-  <script src="{{ asset('static/js/datatable.js') }}"></script>
-@else
-  <div class="card card-mafindo card-body">
+@endif
+@if ($collection->isEmpty())
+  <div class="card card-mafindo card-body mt-3">
     <div class="empty">
       <div class="empty-icon">
         <x-lucide-circle-slash-2 class="icon" />
@@ -89,8 +96,4 @@
     </div>
   </div>
 @endif
-@if (method_exists($collection, 'hasPages') && $collection->hasPages())
-  <div class="mt-3 pt-2 border-top">
-    {{ $collection->links() }}
-  </div>
-@endif
+<script src="{{ asset('static/js/datatable.js') }}"></script>
