9a1fed94749e46471b83ca3b6c935a0c196ad165
Shavana Alif <85614368+krealif@users.noreply.github.com>
2025-01-24 12:09:45 +0000

    enforce HTTPS in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         \Carbon\Carbon::setLocale('id');
 
         Paginator::useBootstrapFive();
