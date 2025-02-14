4bdf8fd17ef2e0cc1e14239f4ef158d2bbb0b7ed
Shavana Alif <85614368+krealif@users.noreply.github.com>
2025-02-14 07:03:14 +0000

    update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\UserDetail;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,38 +14,12 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $admin = User::create([
-            'nama' => 'Admin Mafindo',
-            'email' => 'admin@mail.com',
+            'nama' => 'IT Team',
+            'email' => 'it@mafindo.or.id',
             'is_approved' => true,
             'password' => Hash::make('password'),
         ]);
 
         $admin->assignRole('admin');
-
-        $pengurus = User::create([
-            'nama' => 'Pengurus Mafindo',
-            'email' => 'pengurus@mail.com',
-            'is_approved' => true,
-            'password' => Hash::make('password'),
-            'branch_id' => 1,
-        ]);
-
-        $pengurus->assignRole('pengurus-wilayah');
-
-        $relawan = User::create([
-            'nama' => 'Relawan Mafindo',
-            'email' => 'relawan@mail.com',
-            'is_approved' => true,
-            'password' => Hash::make('password'),
-            'branch_id' => 1,
-        ]);
-
-        UserDetail::create([
-            'user_id' => $relawan->id,
-            'panggilan' => 'Relawan',
-            'tgl_lahir' => '2000-01-01',
-        ]);
-
-        $relawan->assignRole('relawan-wilayah');
     }
 }
